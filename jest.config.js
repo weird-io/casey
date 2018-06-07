1795741fadbe24f97e3ed2c3da45105ee0eef555
Merten van Gerven <merten.vg@gmail.com>
2018-06-07 14:10:42 +0200

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    testRegex: "\\.(test|spec)\\.(jsx?|tsx?)$",
-    testPathIgnorePatterns: ["/lib/", "/node_modules/"],
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    collectCoverage: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "\\.(test|spec)\\.(jsx?|tsx?)$",
+  testPathIgnorePatterns: ["dist", "node_modules", "examples"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true
 };
